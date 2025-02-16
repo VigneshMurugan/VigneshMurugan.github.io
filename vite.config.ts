231b94e127da82170ad73cb1eb4c3e8f040a4245
Vignesh <vignesh328@gmail.com>
2025-02-16 15:49:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/VigneshMurugan.github.io/",
+  base: "/vm-portfolio/",
   server: {
     host: "::",
     port: 8080,
